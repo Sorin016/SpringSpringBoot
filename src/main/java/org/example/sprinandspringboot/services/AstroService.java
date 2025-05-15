a32e35816e0a711582beaddf37fe90ab1d0c4937
Sorin C <sorin.cucereavu@gmail.com>
2025-05-15 17:32:44 +0300

    adaug @Service si unitTest pentru el

---
+++ src/main/java/org/example/sprinandspringboot/services/AstroService.java
@@ -0,0 +1,4 @@
+package org.example.sprinandspringboot.services;
+
+public class AstroService {
+}
---
+++ src/test/java/org/example/sprinandspringboot/services/AstroServiceTest.java
@@ -0,0 +1,15 @@
+package org.example.sprinandspringboot.services;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+public class AstroServiceTest {
+    @Autowired
+    private AstroService astroService;
+    @Test
+    void getPeopleInSpace(){
+        
+    }
+
+}
